8ee027b9f6c78122d564bc04c86bc764b0eb8c54
hana-saku <hana-saku@github.com>
2026-04-12 09:13:39 +0900

    キックボクシング声だしアプリ PWA対応

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'kickboxing-v1';
+const ASSETS = ['./index.html', './manifest.json', './icon.svg', './icon-maskable.svg'];
+
+self.addEventListener('install', ev => {
+  ev.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', ev => {
+  ev.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', ev => {
+  ev.respondWith(
+    caches.match(ev.request).then(r => r || fetch(ev.request).catch(() => caches.match('./index.html')))
+  );
+});
