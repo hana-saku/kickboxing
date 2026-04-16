8f302156987d5f279d83023a7006a25f30c85518
hana-saku <hana-saku@github.com>
2026-04-17 08:22:52 +0900

    updateVoiceBadgeのnullエラー修正

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kickboxing-v23';
+const CACHE = 'kickboxing-v24';
 const ASSETS = ['./index.html', './manifest.json', './icon.svg', './icon-maskable.svg', './gong.mp3', './gong-end.mp3'];
 
 self.addEventListener('install', ev => {
