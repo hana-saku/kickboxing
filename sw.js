36d8cf82afed6ca9017c970c78cba92e3517f140
hana-saku <hana-saku@github.com>
2026-04-17 18:00:48 +0900

    デフォルト音声追加（ElevenLabs 21コール・元気な声）

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'kickboxing-v24';
-const ASSETS = ['./index.html', './manifest.json', './icon.svg', './icon-maskable.svg', './gong.mp3', './gong-end.mp3'];
+const CACHE = 'kickboxing-v25';
+const ASSETS = ['./index.html', './manifest.json', './icon.svg', './icon-maskable.svg', './gong.mp3', './gong-end.mp3', './default-voices.js'];
 
 self.addEventListener('install', ev => {
   ev.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
